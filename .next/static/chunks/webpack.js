b8d93f5a9dc34b43b02660a393a0e01be7aebfda
dev-cujaban <scn.dev.code@gmail.com>
2025-08-20 23:05:32 -0500

    Fixed error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a71d815c6617f16"; }
+/******/ 		__webpack_require__.h = function() { return "ec15db0af55c4332"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
               src="https://www.googletagmanager.com/ns.html?id=GTM-PFK27GSR"
               height="0"
               width="0"
-              style="display:none;visibility:hidden"
+              style={{ display: 'none', visibility: 'hidden' }} 
             ></iframe>
           </noscript>
           {/* End Google Tag Manager (noscript) */}
